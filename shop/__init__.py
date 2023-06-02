4e33db2480085e653d928dde7fea44c2cf993f8c
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-02 11:34:26 +0200

    Inserted register form

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, session, request, redirect, url_for, flash
 
+from .admin.forms import RegistrationForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buyhere.db'
 db = SQLAlchemy(app)
+
+# ---- ROUTES ------
 
 
 @app.route('/')
@@ -13,6 +16,13 @@ def home():
     return "Home page"
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title="Register User")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # user = User(form.username.data, form.email.data,
+        #             form.password.data)
+        # db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title="Registeration Page")

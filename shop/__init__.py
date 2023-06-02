a72345a3ad14f08cc183fc67592059acf1593f39
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-02 13:22:01 +0200

    Refactored routes and added register validators

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,28 +1,8 @@
+from shop.admin import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template, session, request, redirect, url_for, flash
-
-from .admin.forms import RegistrationForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buyhere.db'
+app.config['SECRET_KEY'] = 'secretkey_isse_jaguar'
 db = SQLAlchemy(app)
-
-# ---- ROUTES ------
-
-
-@app.route('/')
-def home():
-    return "Home page"
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        #             form.password.data)
-        # db_session.add(user)
-        flash('Thanks for registering')
-        return redirect(url_for('login'))
-    return render_template('admin/register.html', form=form, title="Registeration Page")
--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,13 +1,25 @@
-from shop import app
+from shop import app, db
+from .forms import RegistrationForm
+from flask import render_template, session, request, redirect, url_for, flash
+from flask_bcrypt import Bcrypt
+from .models import User
+bcrypt = Bcrypt(app)
 
-from flask import render_template, session, request, redirect, url_for
 
-
+# ---- ROUTES ------
 @app.route('/')
 def home():
     return "Home page"
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title="Register User")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data,
+                    password=hash_password)
+        db.session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('home'))
+    return render_template('admin/register.html', form=form, title="Registeration Page")

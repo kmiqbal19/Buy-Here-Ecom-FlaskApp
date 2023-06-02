855c6277c59e6971337dc93a1f8644a1fc33d9c1
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-02 16:19:44 +0200

    Updated add category functionality

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,6 +1,6 @@
 from flask import redirect, render_template, url_for, flash, request, session
 from shop import db, app
-from .models import Brand
+from .models import Brand, Category
 
 # ---- ROUTES ----
 @app.route('/addbrand', methods=['GET', 'POST'])
@@ -15,4 +15,18 @@ def addbrand():
         flash(f'The Brand {getbrand} was added to your database', 'success')
         db.session.commit()
         return redirect(url_for('addbrand'))
-    return render_template('products/addbrand.html', brands='brands')
+    return render_template('products/addbrand.html', brands='brand')
+
+@app.route('/addcategory', methods=['GET', 'POST'])
+def addcategory():
+    if 'email' not in session:
+        flash(f'Please login first', 'danger')
+        return redirect(url_for('login'))
+    if request.method == "POST":
+        getcat = request.form.get('category')
+        cat = Category(name=getcat)
+        db.session.add(cat)
+        flash(f'The Category {getcat} was added to your database', 'success')
+        db.session.commit()
+        return redirect(url_for('addbrand'))
+    return render_template('products/addbrand.html')

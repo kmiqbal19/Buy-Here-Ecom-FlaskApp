e23e9f5cf6519214e9d943f8bc907ecc9087fef8
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-02 15:58:58 +0200

    added add brand html and route and work func

---
+++ shop/products/routes.py
@@ -0,0 +1,18 @@
+from flask import redirect, render_template, url_for, flash, request, session
+from shop import db, app
+from .models import Brand
+
+# ---- ROUTES ----
+@app.route('/addbrand', methods=['GET', 'POST'])
+def addbrand():
+    if 'email' not in session:
+        flash(f'Please login first', 'danger')
+        return redirect(url_for('login'))
+    if request.method == "POST":
+        getbrand = request.form.get('brand')
+        brand = Brand(name=getbrand)
+        db.session.add(brand)
+        flash(f'The Brand {getbrand} was added to your database', 'success')
+        db.session.commit()
+        return redirect(url_for('addbrand'))
+    return render_template('products/addbrand.html', brands='brands')

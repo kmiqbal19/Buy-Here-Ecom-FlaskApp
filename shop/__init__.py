09a1547986f689332accbf1668b3d30f416558d9
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-02 18:05:31 +0200

    Added add product route func + configured upload photos

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buyhere.db'
 app.config['SECRET_KEY'] = 'secretkey_isse_jaguar'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,6 +1,8 @@
 from flask import redirect, render_template, url_for, flash, request, session
-from shop import db, app
-from .models import Brand, Category
+from shop import db, app, photos
+from .models import Brand, Category, Addproduct
+from .forms import Addproducts
+import secrets
 
 # ---- ROUTES ----
 @app.route('/addbrand', methods=['GET', 'POST'])
@@ -18,6 +20,7 @@ def addbrand():
     return render_template('products/addbrand.html', brands='brand')
 
 @app.route('/addcategory', methods=['GET', 'POST'])
+
 def addcategory():
     if 'email' not in session:
         flash(f'Please login first', 'danger')
@@ -46,3 +49,35 @@ def updatebrand(id):
         return redirect(url_for('brands'))
     brand = updatebrand.name
     return render_template('products/updatebrand.html', title='Update brand', brands='brands', updatebrand=updatebrand)
+
+
+@app.route('/addproduct', methods=['POST', 'GET'])
+def addproduct():
+    if 'email' not in session:
+        flash(f'Please login first', 'danger')
+        return redirect(url_for('login'))
+    brands = Brand.query.all()
+    categories = Category.query.all()
+    form = Addproducts(request.form)
+    if request.method == "POST":
+        name = form.name.data
+        price = form.price.data
+        discount = form.discount.data
+        stock = form.stock.data
+        colors = form.colors.data
+        desc = form.description.data
+        brand = request.form.get('brand')
+        category = request.form.get('category')
+        image_1 = photos.save(request.files.get('image_1'), name=secrets.token_hex(10) + ".")
+        image_2 = photos.save(request.files.get('image_2'), name=secrets.token_hex(10) + ".")
+        image_3 = photos.save(request.files.get('image_3'), name=secrets.token_hex(10) + ".")
+        addproduct = Addproduct(name=name, price=price, discount=discount, stock=stock, colors=colors, desc=desc,
+                                category_id=category, brand_id=brand,
+        image_1=image_1, image_2=image_2, image_3=image_3)
+        db.session.add(addproduct)
+        flash(f'The product {name} was added in database', 'success')
+        db.session.commit()
+        return redirect(url_for('admin'))
+
+    return render_template('products/addproduct.html', form=form, title='Add Product', brands=brands,
+                           categories=categories)

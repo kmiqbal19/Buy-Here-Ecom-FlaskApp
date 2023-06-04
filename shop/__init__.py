d2e54726e4db1696e320541366b95881e81c4fb0
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-04 15:42:07 +0200

    added cart route

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 import os
+from flask_msearch import Search
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -15,6 +16,9 @@ configure_uploads(app, photos)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+search = Search()
+search.init_app(app)
 
 from shop.admin import routes
 from shop.products import routes
+from shop.carts import carts

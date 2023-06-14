cbe243815408054f2bb972a834f978c326153ccd
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-14 20:58:03 +0200

    added customer order model

--- shop/customers/model.py
+++ shop/customers/model.py
@@ -1,3 +1,4 @@
+import json
 from shop import app, db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
@@ -36,6 +37,31 @@ class Register(db.Model, UserMixin):
     
     def __repr__(self):
         return '<Register %r>' % self.name
+
+class JsonEcodedDict(db.TypeDecorator):
+    impl = db.Text
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return {}
+        else:
+            return json.loads(value)
+
+class CustomerOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    invoice = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(20), default='Pending', nullable=False)
+    customer_id = db.Column(db.Integer, unique=False, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    orders = db.Column(JsonEcodedDict)
+
+    def __repr__(self):
+        return'<CustomerOrder %r>' % self.invoice
+
 
 with app.app_context():
     db.create_all()

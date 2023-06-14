ab8d158b9b0068eae49838b7fe21f86d28d08c74
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-14 21:07:44 +0200

    finished order now functionality

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, url_for, flash, request, session, c
 from flask_login import login_required, current_user, logout_user, login_user
 from shop import db, app, photos, search, bcrypt , login_manager
 from .forms import CustomerRegisterForm , CustomerLoginFrom
-from .model import Register
+from .model import Register, CustomerOrder
 import secrets
 import os
 
@@ -38,3 +38,29 @@ def customerLogin():
 def customer_logout():
     logout_user()
     return redirect(url_for('home'))
+
+def updateshoppingcart():
+    for key, shopping in session['Shoppingcart'].items():
+        session.modified = True
+        del shopping['image']
+        del shopping['colors']
+    return updateshoppingcart
+
+@app.route('/getorder')
+@login_required
+def get_order():
+    if current_user.is_authenticated:
+        customer_id = current_user.id
+        invoice = secrets.token_hex(5)
+        updateshoppingcart
+        try:
+            order = CustomerOrder(invoice=invoice,customer_id=customer_id,orders=session['Shoppingcart'])
+            db.session.add(order)
+            db.session.commit()
+            session.pop('Shoppingcart')
+            flash('Your order has been sent successfully','success')
+            return redirect(url_for('orders',invoice=invoice))
+        except Exception as e:
+            print(e)
+            flash('Some thing went wrong while get order!', 'danger')
+            return redirect(url_for('getCart'))

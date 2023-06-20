d862459e60337e25689e896e58b03748b6806828
kmiqbal19 <km.iqbal01@gmail.com>
2023-06-20 15:33:47 +0200

    added image i customer page

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -77,7 +77,7 @@ def customer_logout():
 def updateshoppingcart():
     for key, shopping in session['Shoppingcart'].items():
         session.modified = True
-        del shopping['image']
+        # del shopping['image']
         del shopping['colors']
     return updateshoppingcart
 
